e6e6d305f281f48497268bb4b419bc5824d1f9e7
Jean de Klerk <jadekler@gmail.com>
2016-06-12 20:29:20 -0600

    Update the app for pushing to CF, including manifest and using

--- integration_test.go
+++ integration_test.go
@@ -4,11 +4,17 @@ import (
     "testing"
     "net/http"
     "fmt"
+    "os"
 )
 
 // "go run main.go" first
 func TestIntegration(t *testing.T) {
-    resp, err := http.Get("http://localhost:8080")
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    resp, err := http.Get(fmt.Sprintf("http://localhost:%s", port))
 
     if err != nil || resp.StatusCode != 200 {
         fmt.Printf("Failed with resp %v error %v \n", resp, err)
--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
     "net/http"
     "fmt"
+    "os"
 )
 
 func main() {
-    fmt.Println("Listening on port 8080")
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    fmt.Printf("Listening on port %s\n", port)
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Hello world"))
     })
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
